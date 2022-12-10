53b457296e10d843f32f916d0464e0b0c9a59a80
Tiffany McNerlin <tiffanymcnerlin@gmail.com>
2022-12-10 13:05:55 -0500

    installed/configured tailwind & next/fonts

--- components/Masthead.tsx
+++ components/Masthead.tsx
@@ -4,37 +4,37 @@ import { Raleway } from "@next/font/google";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const font = Raleway({
-  subsets: ["latin"],
+	subsets: ["latin"],
 });
 
 const Masthead: React.FC = () => {
-  return (
-    <div className={font.className}>
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <video
-          autoPlay
-          loop
-          muted
-          playsInline
-          className="absolute h-full w-full object-cover mix-blend-multiply"
-        >
-          <source src="/jellyfish-masthead.mp4" type="video/mp4" />
-        </video>
+	return (
+		<div className={font.className}>
+			<div className="flex min-h-screen flex-col items-center justify-center">
+				<video
+					autoPlay
+					loop
+					muted
+					playsInline
+					className="absolute h-full w-full object-cover mix-blend-multiply"
+				>
+					<source src="/jellyfish-masthead.mp4" type="video/mp4" />
+				</video>
 
-        <div className="z-10 flex flex-1 flex-col items-center justify-center p-12 text-center font-bold text-white drop-shadow-[0_5px_3px_rgba(0,0,0,4)]">
-          <h1 className="mb-6 text-4xl xl:text-5xl">Jellies</h1>
-          <h2 className="mb-2 text-2xl tracking-tight xl:text-3xl">
-            <span></span>
-          </h2>
-        </div>
-        <div className="pb-30 flex-grow-0 mix-blend-multiply transition-all duration-1000 md:pb-10">
-          <KeyboardArrowDownIcon
-            sx={{ width: 80, height: 80, color: "text.disabled" }}
-          />
-        </div>
-      </div>
-    </div>
-  );
+				<div className="z-10 flex flex-1 flex-col items-center justify-center p-12 text-center font-bold text-white drop-shadow-[0_5px_3px_rgba(0,0,0,4)]">
+					<h1 className="mb-6 text-4xl xl:text-5xl">Jellies</h1>
+					<h2 className="mb-2 text-2xl tracking-tight xl:text-3xl">
+						<span></span>
+					</h2>
+				</div>
+				<div className="pb-30 flex-grow-0 mix-blend-multiply transition-all duration-1000 md:pb-10">
+					<KeyboardArrowDownIcon
+						sx={{ width: 80, height: 80, color: "text.disabled" }}
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Masthead;
--- pages/index.tsx
+++ pages/index.tsx
@@ -6,34 +6,24 @@ import Masthead from "../components/Masthead";
 import About from "../components/About";
 
 export default function Home() {
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Jellies</title>
-        <meta
-          name="description"
-          content="General landing page with jellyfish as the topic"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+				<title>Jellies</title>
+				<meta
+					name="description"
+					content="General landing page with jellies as the topic"
+				/>
+			</Head>
 
-      <main>
-        <Masthead />
-        <About />
-      </main>
+			<main>
+				<Masthead />
+				<About />
+			</main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
-  );
+			<footer className={styles.footer}>
+				Powered by <span className={styles.logo}></span>
+			</footer>
+		</div>
+	);
 }
